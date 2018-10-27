a61576c8ead171a8bd9006b8eb7e9b43331dcbf2
Olah Tamas <tomi@192-168-0-100.rdsnet.ro>
2018-10-27 15:44:34 +0300

    Small fixes

--- src/main/java/com/anonymization/repositories/MedicalRecordRepository.java
+++ src/main/java/com/anonymization/repositories/MedicalRecordRepository.java
@@ -2,7 +2,7 @@ package com.anonymization.repositories;
 
 import com.anonymization.model.MedicalRecord;
 import org.springframework.data.cassandra.repository.CassandraRepository;
-
+import org.springframework.stereotype.Repository;
 
 public interface MedicalRecordRepository extends CassandraRepository<MedicalRecord,Integer> {
 }
--- src/main/java/com/anonymization/repositories/ReportRepository.java
+++ src/main/java/com/anonymization/repositories/ReportRepository.java
@@ -2,6 +2,7 @@ package com.anonymization.repositories;
 
 import com.anonymization.model.Report;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 public interface ReportRepository extends CassandraRepository<Report,Integer> {
 }
